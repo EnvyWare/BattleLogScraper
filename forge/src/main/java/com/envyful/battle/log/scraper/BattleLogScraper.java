601e861831702d9025f26954a59d26762ce311a4
Daniel Norris <dnorris443@gmail.com>
2023-08-29 11:58:17 +0100

    chore(gradle): bump version to 1.5.0

--- forge/src/main/java/com/envyful/battle/log/scraper/BattleLogScraper.java
+++ forge/src/main/java/com/envyful/battle/log/scraper/BattleLogScraper.java
@@ -21,7 +21,7 @@ public class BattleLogScraper {
 
     private static BattleLogScraper instance;
 
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.5.0";
 
     private BattleLogScraperConfig config;
 
