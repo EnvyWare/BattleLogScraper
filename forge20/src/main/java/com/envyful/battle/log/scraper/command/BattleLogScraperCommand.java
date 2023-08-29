57686310a2462db4eb3f4b66f53d0e4a19c795af
Daniel Norris <dnorris443@gmail.com>
2023-08-29 12:03:55 +0100

    feat(forge20): add reload command

---
+++ forge20/src/main/java/com/envyful/battle/log/scraper/command/BattleLogScraperCommand.java
@@ -0,0 +1,25 @@
+package com.envyful.battle.log.scraper.command;
+
+import com.envyful.api.command.annotate.Command;
+import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Sender;
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.battle.log.scraper.BattleLogScraper;
+import net.minecraft.commands.CommandSource;
+
+@Command(
+        value = "battlelogscraper",
+        aliases = {
+                "battlelogscraperreload"
+        }
+)
+@Permissible("com.envyful.scraper.reload")
+public class BattleLogScraperCommand {
+
+    @CommandProcessor
+    public void onCommand(@Sender CommandSource sender, String[] args) {
+        BattleLogScraper.reloadConfig();
+        sender.sendSystemMessage(UtilChatColour.colour("Reloaded configs"));
+    }
+}
